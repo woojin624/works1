b2b00204eb3d2c188dd628d0a8bcd246778542c4
woojin624 <54699702+woojin624@users.noreply.github.com>
2020-03-13 18:54:44 +0900

    trigger

--- Ezweb_ScrollTrigger/script.js
+++ Ezweb_ScrollTrigger/script.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
-  var trigger = new ScrollTrigger();
+  var trigger = new ScrollTrigger({
+    offset: {
+      x: 0,
+      y: 250
+    },
+    toggle: {
+      visible: "animateIn",
+      hidden: "animateOut"
+    }
+  });
 });
