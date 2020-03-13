0d4f569358218a460716affa36ee1b501a855b47
woojin624 <54699702+woojin624@users.noreply.github.com>
2020-03-14 04:52:08 +0900

    slideshow finish

--- Ez_SlideShow/script.js
+++ Ez_SlideShow/script.js
@@ -32,8 +32,49 @@ for (var i = 0; i < slideCount; i++) {
 
 // 슬라이드 이동 함수
 
+function goToSlide(idx) {
+  sliderContainer[0].style.left = idx * -100 + "%";
+  sliderContainer[0].classList.add("animated");
+  currentIndex = idx;
+
+  //   updateNav();
+}
+
 // 버튼기능 업데이트 함수
+function updateNav() {
+  //처음일때
+  if (currentIndex == 0) {
+    navPrev.classList.add("disabled");
+  } else {
+    navPrev.classList.remove("disabled");
+  }
+  //마지막일때
+  if (currentIndex == slideCount - 1) {
+    navNext.classList.add("disabled");
+  } else {
+    navNext.classList.remove("disabled");
+  }
+}
 
 // 버튼을 클릭하면 슬라이드 이동시키기
 
+navPrev.addEventListener("click", function(event) {
+  event.preventDefault();
+  if (currentIndex > 0) {
+    goToSlide(currentIndex - 1);
+  } else {
+    goToSlide(slideCount - 1);
+  }
+});
+
+navNext.addEventListener("click", function(event) {
+  event.preventDefault();
+  if (currentIndex < slideCount - 1) {
+    goToSlide(currentIndex + 1);
+  } else {
+    goToSlide(0);
+  }
+});
+
 // 첫번째 슬라이드 먼저 보이도록 하기
+goToSlide(0);
