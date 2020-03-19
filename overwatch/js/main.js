f9bddbffadbca80af49b4312ec232c236a5f0b2b
woojin624 <54699702+woojin624@users.noreply.github.com>
2020-03-20 06:31:05 +0900

    over main..

---
+++ overwatch/js/main.js
@@ -0,0 +1,22 @@
+// const mainNav = document.getElementsByClassName(".main_nav");
+
+// mainNav.addEventListener("mouseenter", function() {
+//   document.getElementsByClassName(".main_nav_open").classList.add(".nav_show");
+// });
+// mainNav.addEventListener("mouseleave", function() {
+//   document
+//     .getElementsByClassName(".main_nav_open")
+//     .classList.remove(".nav_show");
+// });
+
+// const mainNav = document.querySelector(".main_nav");
+// const mno = document.querySelector(".main_nav_open");
+
+// mainNav.addEventListener("mouseenter", function() {
+//   const showClass = "nav-show";
+//   mno.classList.add(showClass);
+
+//   this.addEventListener("mouseleave", function() {
+//     mno.classList.remove(showClass);
+//   });
+// });
