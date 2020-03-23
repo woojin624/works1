5f7ecb5605181294210f09b2c0f7788ced36ebb7
woojin624 <54699702+woojin624@users.noreply.github.com>
2020-03-24 05:07:28 +0900

    over not

--- PUBG-edit/js/slider-nav.js
+++
@@ -1,8 +0,0 @@
-var SliderContainer = document.getElementsByClassName('slider-container'),
-    nav = document.getElementsByClassName('type'); //type list
-
-    for(var a = 0; a < nav.length; a++){
-        nav[a].addEventListener('click', funtcion(e){
-            
-        });
-    };
