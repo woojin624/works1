b0fe886f0e18ef7408b66bd082930256c640c502
woojin624 <54699702+woojin624@users.noreply.github.com>
2020-03-23 15:24:21 +0900

    button

--- overwatch/js/videocontrol.js
+++ overwatch/js/videocontrol.js
@@ -5,16 +5,11 @@ var btnCover = document.getElementsByClassName("button_cover");
 
 vPlay.addEventListener("click", function() {
   myVideo.play();
+  vPlay.style.display = "none";
+  vPause.style.display = "flex";
 });
 vPause.addEventListener("click", function() {
   myVideo.pause();
+  vPause.style.display = "none";
+  vPlay.style.display = "flex";
 });
-
-// for (var i = 0; i < btnCover.length; i++) {
-// for (var j = 0; j < btnCover.length; j++) {
-//   btnCover[j].addEventListener("click", function(ev) {
-//     btnCover[j].classList.remove("button_show");
-//     ev.target.nextSibling.classList.add("button_show");
-//   });
-// }
-// }
