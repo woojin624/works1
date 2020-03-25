ee4e655f0dbbc7500d284f52d77a2a6b157a0a92
woojin624 <54699702+woojin624@users.noreply.github.com>
2020-03-26 04:43:27 +0900

    over side nav

--- overwatch/js/mobile_nav_side.js
+++ overwatch/js/mobile_nav_side.js
@@ -1,34 +1,55 @@
 var mnsPanel = document.getElementsByClassName("mns_panel"),
   panelHeading = document.getElementsByClassName("mns_panel_heading"),
-  panelBody = document.getElementsByClassName("mns_panel_body"),
-  openBtn = document.getElementsByClassName("mns_openBtn"),
-  closeBtn = document.getElementsByClassName("mns_clostBtn"),
-  mns = document.getElementsByClassName("mobile_nav_side");
+  panelBody = document.getElementsByClassName("mns_panel_body");
 
-const toggleBtn = document.querySelector(".navbar_togleBtn");
-const menu = document.querySelector(".navbar_menu");
-const icons = document.querySelector(".navbar_icons");
+const openBtn = document.getElementById("mns_openBtn");
+const closeBtn = document.getElementById("mns_closeBtn");
+const mns = document.querySelector(".mobile_nav_side");
+const ovarlay = mns.querySelector(".mns_overlay");
+const openMns = () => {
+  mns.classList.remove("hidden");
+  ovarlay.classList.remove("mnso_hidden");
+};
+const closeMns = () => {
+  mns.classList.add("hidden");
+  ovarlay.classList.add("mnso_hidden");
+};
+ovarlay.addEventListener("click", closeMns);
+openBtn.addEventListener("click", openMns);
+closeBtn.addEventListener("click", closeMns);
 
-openBtn.addEventListener("click", () => {
-  mns.classList.toggle("active3");
-});
+const openBtn2 = document.getElementById("mas_openBtn");
+const closeBtn2 = document.getElementById("mas_closeBtn");
+const mas = document.querySelector(".mobile_account_side");
+const ovarlay2 = mas.querySelector(".mas_overlay");
+const openMas = () => {
+  mas.classList.remove("hidden_man");
+  ovarlay2.classList.remove("maso_hidden");
+};
+const closeMas = () => {
+  mas.classList.add("hidden_man");
+  ovarlay2.classList.add("maso_hidden");
+};
+ovarlay2.addEventListener("click", closeMas);
+openBtn2.addEventListener("click", openMas);
+closeBtn2.addEventListener("click", closeMas);
 
-// for (var i = 0; i < panelHeading.length; i++) {
-//   panelHeading[i].addEventListener("click", function(ev) {
-//     for (var j = 0; j < mnsPanel.length; j++) {
-//       mnsPanel[j].classList.remove("active2");
-//       ev.target.parentNode.classList.add("active2");
-//       activateBody2();
-//     }
-//   });
-// }
-// function activateBody2() {
-//   for (var x = 0; x < panelBody.length; x++) {
-//     panelBody[x].style.display = "none";
-//   }
-//   var activePanel2 = document.querySelector(
-//     ".mns_panel.active2 .mns_panel_body"
-//   );
-//   activePanel2.style.display = "block";
-// }
-// activateBody2();
+for (var i = 0; i < panelHeading.length; i++) {
+  panelHeading[i].addEventListener("click", function(ev) {
+    for (var j = 0; j < mnsPanel.length; j++) {
+      mnsPanel[j].classList.remove("active2");
+      ev.target.parentNode.classList.add("active2");
+      activateBody2();
+    }
+  });
+}
+function activateBody2() {
+  for (var x = 0; x < panelBody.length; x++) {
+    panelBody[x].style.display = "none";
+  }
+  var activePanel2 = document.querySelector(
+    ".mns_panel.active2 .mns_panel_body"
+  );
+  activePanel2.style.display = "block";
+}
+activateBody2();
